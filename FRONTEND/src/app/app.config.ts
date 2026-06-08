f3cb1d58961502f054f8770d6a7fe59054423e2e
Kuswanth <308324@ust.com>
2026-06-08 11:47:51 +0530

    file name changed

--- FRONTEND/src/app/app.config.ts
+++ FRONTEND/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient,withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './core/interceptors/token-interceptor';
 
 import { routes } from './app.routes';
 
