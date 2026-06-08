c3511ea2560b79be87a789636ef121585a50443d
Kuswanth <308324@ust.com>
2026-06-08 12:22:23 +0530

    chnaged routes.ts

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,68 +1,75 @@
 import { Routes } from '@angular/router';
-
-import { Landing } from './features/landing/landing';
-import { Login } from './features/auth/login/login';
-import { Register } from './features/auth/register/register';
-import { ResetPassword } from './features/auth/reset-password/reset-password';
-import { AccountInactive } from './features/auth/account-inactive/account-inactive';
-import { Dashboard } from './features/dashboard/dashboard';
-
-import { EmployeeList } from './features/employee/employee-list/employee-list';
-import { PatientList } from './features/patient/patient-list/patient-list';
-import { AppointmentList } from './features/appointments/appointment-list/appointment-list';
-import { Profile } from './features/profile/profile';
-
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Landing
+        loadComponent: () =>
+            import('./features/landing/landing').then((m) => m.Landing),
     },
     {
         path: 'login',
-        component: Login
+        loadComponent: () =>
+            import('./features/auth/login/login').then((m) => m.Login),
     },
     {
         path: 'register',
-        component: Register
+        loadComponent: () =>
+            import('./features/auth/register/register').then((m) => m.Register),
     },
     {
         path: 'reset-password',
-        component: ResetPassword,
-        canActivate: [authGuard]
+        loadComponent: () =>
+            import('./features/auth/reset-password/reset-password').then(
+                (m) => m.ResetPassword
+            ),
+        canActivate: [authGuard],
     },
     {
         path: 'account-inactive',
-        component: AccountInactive
+        loadComponent: () =>
+            import('./features/auth/account-inactive/account-inactive').then(
+                (m) => m.AccountInactive
+            ),
     },
     {
         path: 'dashboard',
-        component: Dashboard,
-        canActivate: [authGuard]
+        loadComponent: () =>
+            import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+        canActivate: [authGuard],
     },
     {
         path: 'employees',
-        component: EmployeeList,
-        canActivate: [authGuard]
+        loadComponent: () =>
+            import('./features/employee/employee-list/employee-list').then(
+                (m) => m.EmployeeList
+            ),
+        canActivate: [authGuard],
     },
     {
         path: 'patients',
-        component: PatientList,
-        canActivate: [authGuard]
+        loadComponent: () =>
+            import('./features/patient/patient-list/patient-list').then(
+                (m) => m.PatientList
+            ),
+        canActivate: [authGuard],
     },
     {
         path: 'appointments',
-        component: AppointmentList,
-        canActivate: [authGuard]
+        loadComponent: () =>
+            import(
+                './features/appointments/appointment-list/appointment-list'
+            ).then((m) => m.AppointmentList),
+        canActivate: [authGuard],
     },
     {
         path: 'profile',
-        component: Profile,
-        canActivate: [authGuard]
+        loadComponent: () =>
+            import('./features/profile/profile').then((m) => m.Profile),
+        canActivate: [authGuard],
     },
     {
         path: '**',
-        redirectTo: ''
-    }
+        redirectTo: '',
+    },
 ];
