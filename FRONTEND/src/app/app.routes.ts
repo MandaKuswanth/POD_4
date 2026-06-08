72051586c7b8bd6569ad9523c0686f97e681fee9
Kuswanth <308324@ust.com>
2026-06-08 12:20:48 +0530

    added app.routes.ts

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,3 +1,68 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Landing } from './features/landing/landing';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { ResetPassword } from './features/auth/reset-password/reset-password';
+import { AccountInactive } from './features/auth/account-inactive/account-inactive';
+import { Dashboard } from './features/dashboard/dashboard';
+
+import { EmployeeList } from './features/employee/employee-list/employee-list';
+import { PatientList } from './features/patient/patient-list/patient-list';
+import { AppointmentList } from './features/appointments/appointment-list/appointment-list';
+import { Profile } from './features/profile/profile';
+
+import { authGuard } from './core/guards/auth-guard';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Landing
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'register',
+        component: Register
+    },
+    {
+        path: 'reset-password',
+        component: ResetPassword,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'account-inactive',
+        component: AccountInactive
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'employees',
+        component: EmployeeList,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'patients',
+        component: PatientList,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'appointments',
+        component: AppointmentList,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'profile',
+        component: Profile,
+        canActivate: [authGuard]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
