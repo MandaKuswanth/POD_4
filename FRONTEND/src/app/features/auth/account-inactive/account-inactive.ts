4b8b5ee9276c36c5edaf6c17135b4b13dbc92bc8
Kuswanth <308324@ust.com>
2026-06-08 10:36:12 +0530

    added folder structure

---
+++ FRONTEND/src/app/features/auth/account-inactive/account-inactive.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-inactive',
+  imports: [],
+  templateUrl: './account-inactive.html',
+  styleUrl: './account-inactive.css',
+})
+export class AccountInactive {}
---
+++ FRONTEND/src/app/features/patient/patient-dialog/patient-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-dialog',
+  imports: [],
+  templateUrl: './patient-dialog.html',
+  styleUrl: './patient-dialog.css',
+})
+export class PatientDialog {}
