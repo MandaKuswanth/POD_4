7c3ebc916241b21f12921143ca40c31d85e5976c
Kuswanth <308324@ust.com>
2026-06-08 12:02:13 +0530

    added prebuilt thems to css

--- FRONTEND/src/app/app.config.ts
+++ FRONTEND/src/app/app.config.ts
@@ -1,13 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient,withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token-interceptor';
-
+import { provideToastr } from 'ngx-toastr'
 import { routes } from './app.routes';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true
+    }),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideNativeDateAdapter()
   ]
 };
